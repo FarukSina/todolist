b8f95cb8de4a205ea4cabce050b9b9bd770c94ad
FarukSina <200437201@student.georgianc.on.ca>
2019-12-25 14:49:22 -0500

    Updates

---
+++ precache-manifest.ece41e5b8294627465513d3cc70a11b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec9e0b1b4b961deb098e544faa614dc4",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "a193e46665821fe3bd56",
+    "url": "/todolist/static/css/main.132fc1c3.chunk.css"
+  },
+  {
+    "revision": "0d50be54ad12bbb7b0e7",
+    "url": "/todolist/static/js/2.5114121a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todolist/static/js/2.5114121a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a193e46665821fe3bd56",
+    "url": "/todolist/static/js/main.9457e046.chunk.js"
+  },
+  {
+    "revision": "507716673e279f58353e",
+    "url": "/todolist/static/js/runtime-main.948c6c71.js"
+  }
+]);
